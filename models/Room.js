f54467fc9964efbbc4f0e5d81b33f5200986c78f
Ali Roohan <aliroohan321@gmail.com>
2025-04-21 13:28:36 +0500

    Added all files

--- models/Room.js
+++ models/Room.js
@@ -6,18 +6,10 @@ const roomSchema = new mongoose.Schema({
     required: true,
     trim: true
   },
-  users: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
   latestMessage: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Message'
   },
-  admin: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }
 }, {
   timestamps: true
 });
